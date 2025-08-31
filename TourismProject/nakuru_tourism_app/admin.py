8e7dda655ac730077d7c45c7472ae979651fcdea
nangami29 <rachaelnangami29@gmail.com>
2025-08-31 12:25:32 +0300

    tagging functionality and templates

--- TourismProject/nakuru_tourism_app/admin.py
+++ TourismProject/nakuru_tourism_app/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import CustomUser, Attraction, Category, Rating
-
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 admin.site.register(CustomUser)
-admin.site.register(Attraction)
+
 admin.site.register(Category)
 admin.site.register(Rating)
+@admin.register(Attraction)
+class AttractionAdmin(ImportExportModelAdmin):
+    list_display = ('name', 'category', 'location', 'entry_fee')
+    search_fields = ('name', 'category')
--- TourismProject/nakuru_tourism_app/models.py
+++ TourismProject/nakuru_tourism_app/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
+from taggit.managers import TaggableManager
+
+# Integrate Tagging Functionality
+class Tag(models.Model):
+    name = models.CharField(max_length = 50, unique=True)
+
+    def __str__(self):
+        return self.name
+    
 # Create your models here.
 class CustomUser (AbstractUser):
    Role_CHOICES = (
@@ -28,7 +37,12 @@ class Attraction (models.Model):
     opening_hours =models.CharField (max_length=50, null=True, blank=True)
     created_at= models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='attractions')
-    image= models.ImageField(upload_to='attractions/')
+    image= models.ImageField(upload_to='attractions/', blank=True)
+    tags =TaggableManager()
+
+    def __str__(self):
+        return self.name
+
     def get_absolute_url(self):
         return reverse("attraction_detail", kwargs={"pk": self.pk})
 class Rating (models.Model):
